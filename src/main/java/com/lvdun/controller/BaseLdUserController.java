37771fc7e163953821158909e9535008580838ab
james_2017 <15326922@qq.com>
2017-06-20 14:43:27 +0800

    test

--- src/main/java/com/lvdun/controller/BaseLdUserController.java
+++ src/main/java/com/lvdun/controller/BaseLdUserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -27,8 +28,21 @@ public class BaseLdUserController {
         //Long customerId = Long.parseLong("" + loginUser.get("customerId"));
         Long customerId = 1L;
 
-        Map result = baseLdUserService.getBaseLdUserPage(customerId, page, pageSize);
-        System.out.println(JSON.toJSONString(result));
-        return JSON.toJSON(result);
+        Map resutltMap = new HashMap();
+        int isSuccess = 0;
+        Map result = new HashMap();
+
+        try {
+            result = baseLdUserService.getBaseLdUserPage(customerId, page, pageSize);
+            isSuccess = 1;
+        } catch (Exception e) {
+            isSuccess = 0;
+            e.printStackTrace();
+        }
+
+        resutltMap.put("isSuccess", isSuccess);
+        resutltMap.put("result", result);
+        System.out.println(JSON.toJSONString(resutltMap));
+        return JSON.toJSON(resutltMap);
     }
 }
